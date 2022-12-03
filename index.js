128e98719a2c96c5fa78cfbbf8b2b2e7858e0450
= <nils.ritze@icloud.com>
2022-12-03 18:43:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import { lon, lat } from './issAPI.js';
+
+function initMap(Lat, Lon) {
+    const iss = { lat: Lat, lng: Lon };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 2,
+        center: iss,
+        mapTypeId: "satellite",
+    });
+
+    const marker = new google.maps.Marker({
+        position: iss,
+        map: map,
+        title: "ISS",
+        label: "ISS", 
+    });
+
+    
+  }
+
+window.initMap = initMap;
+
+window.addEventListener('DOMContentLoaded', () => {
+    initMap(10.0, 10.0)
+})
---
+++ issAPI.js
@@ -0,0 +1,19 @@
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var request = new XMLHttpRequest();
+let lat, lon;
+
+request.open('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+request.onreadystatechange = function()
+{
+    if (request.readyState === 4)
+    {
+        var data = JSON.parse(request.responseText);
+        lat = data.latitude;
+        lon = data.longitude;
+    }
+}
+
+export { lat, lon };
+
+request.send();
