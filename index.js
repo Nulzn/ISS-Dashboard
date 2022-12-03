963f7f2d99c89974c97452fd58f0bafd741b168f
= <nils.ritze@icloud.com>
2022-12-03 20:34:44 +0100

    Changes V0.2

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { lon, lat } from './issAPI.js';
-
-function initMap(Lat, Lon) {
+function initMap(Lat, Lng) {
     const iss = { lat: Lat, lng: Lon };
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 2,
